ea0e3e97cc7c61bc6966171bf66aaa054ac1d069
zhangyang <15210801604@163.com>
2018-10-11 22:08:14 +0800

    master

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import {addGUN } from './index.redux'
 class App extends React.Component{
     /*constructor(props){
         super(props)
@@ -8,10 +7,15 @@ class App extends React.Component{
     render(){
         const store = this.props.store
         const num = store.getState()
+        const addGun = this.props.addGun
+        const removeGun = this.props.removeGun
+        const addGunAsync = this.props.addGunAsync
         return (
             <div>
                  <h1>现在有{num}把</h1>
-                <button onClick={()=>store.dispatch(addGUN())}>申请武器</button>
+                <button onClick={()=>store.dispatch(addGun())}>申请武器</button>
+                <button onClick={()=>store.dispatch(removeGun())}>回收武器</button>
+                <button onClick={()=>store.dispatch(addGunAsync())}>推两天</button>
             </div>    )
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-import {createStore} from 'redux'
-
+import {createStore, applyMiddleware,compose} from 'redux'
+import thunk from 'redux-thunk'
 import App from './App'
-import { counter } from './index.redux'
+import { counter ,addGun, removeGun,addGunAsync} from './index.redux'
 
-const store = createStore(counter)
+const store = createStore(counter,compose(applyMiddleware(thunk),window.devToolsExtension?window.devToolsExtension():f=>f))
 
 function render(){
-    ReactDom.render(<App store={store}/>,document.getElementById('root'))
+    ReactDom.render(<App store={store} addGun={addGun} removeGun={removeGun} addGunAsync={addGunAsync}/>,document.getElementById('root'))
 }
 
 render()
