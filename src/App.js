3f075dd257aa3f4a6eaa44e8cbe89eae9fbe3b26
zhangyang <15210801604@163.com>
2018-10-21 20:25:03 +0800

    router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {addGun, removeGun, addGunAsync} from './index.redux'
 class App extends React.Component{
     /*constructor(props){
         super(props)
     }*/
 
     render(){
-        const store = this.props.store
-        const num = store.getState()
+        const num = this.props.num
         const addGun = this.props.addGun
         const removeGun = this.props.removeGun
         const addGunAsync = this.props.addGunAsync
         return (
             <div>
                  <h1>现在有{num}把</h1>
-                <button onClick={()=>store.dispatch(addGun())}>申请武器</button>
-                <button onClick={()=>store.dispatch(removeGun())}>回收武器</button>
-                <button onClick={()=>store.dispatch(addGunAsync())}>推两天</button>
+                <button onClick={addGun}>申请武器</button>
+                <button onClick={removeGun}>回收武器</button>
+                <button onClick={addGunAsync}>推两天</button>
             </div>    )
     }
 }
+const mapStatetoProps=(state)=>{
+    return {num:state}
+}
 
+const actionCreators = {
+    addGun, removeGun, addGunAsync}
+
+App = connect(mapStatetoProps,actionCreators)(App)
 export default App
--- src/index.js
+++ src/index.js
@@ -2,13 +2,64 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import {createStore, applyMiddleware,compose} from 'redux'
 import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import {BrowserRouter, Route, Link,Redirect, Switch} from 'react-router-dom'
 import App from './App'
-import { counter ,addGun, removeGun,addGunAsync} from './index.redux'
+import { counter} from './index.redux'
 
-const store = createStore(counter,compose(applyMiddleware(thunk),window.devToolsExtension?window.devToolsExtension():f=>f))
+const store = createStore(counter,compose(
+    applyMiddleware(thunk),window.devToolsExtension?window.devToolsExtension():f=>f)
+)
+function Erying() {
+    return <h2>二营</h2>
+}
+function Qibinglian() {
+    return <h2>骑兵连</h2>
+}
 
+class Test extends React.Component{
+    constructor(props){
+        super(props)
+    }
+    render(){
+        console.log(this.props)
+        return <h2>测试组件{this.props.match.params.location}</h2>
+    }
+}
 function render(){
-    ReactDom.render(<App store={store} addGun={addGun} removeGun={removeGun} addGunAsync={addGunAsync}/>,document.getElementById('root'))
+    ReactDom.render(
+        (<Provider  store={store}>
+            <BrowserRouter>
+                <div>
+                    <ul>
+                        <li>
+                            <Link to='/'>一</Link>
+                        </li>
+                        <li>
+                            <Link to='/erying'>二</Link>
+                        </li>
+                        <li>
+                            <Link to='/qibinglian'>三</Link>
+                        </li>
+                    </ul>
+                    <Switch>
+                        {/*swtich之渲染命中的第一个root*/}
+                        {/*<Redirect to='/qibinglian' />*/}
+                        <Route path='/' exact component={App}/>
+                        <Route path='/erying' component={Erying}/>
+                        <Route path='/qibinglian' component={Qibinglian}/>
+                        <Route path='/:location' exact component={Test}/>
+                    </Switch>
+
+
+                </div>
+
+
+            </BrowserRouter>
+
+        </Provider> ),
+        document.getElementById('root')
+    )
 }
 
 render()
